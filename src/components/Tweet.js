008f1d8b22affa05eff4e4cef067737e4dc37c57
kalush81 <krzysiek.kaluza@gmail.com>
2020-07-06 16:08:47 +0200

    Install and test react-icons

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { formatTweet } from "../utils/helpers";
+import {
+  TiArrowBackOutline,
+  TiHeartFullOutline,
+  TiHeartOutline,
+} from "react-icons/ti";
 
 export default function Tweet({ tweetId }) {
   const tweet = useSelector(({ tweets, users, authedUser }) => {
     const tweet = tweets[tweetId];
-    const parrent = tweet.replyingTo ? tweets[tweet.replyingTo] : null
+    const parrent = tweet.replyingTo ? tweets[tweet.replyingTo] : null;
     return {
       tweet: formatTweet(tweet, users[tweet.author], authedUser, parrent),
     };
@@ -17,6 +22,9 @@ export default function Tweet({ tweetId }) {
     <div>
       <h2>{tweet.tweet.name}</h2>
       <p>{tweet.tweet.text}</p>
+      <TiArrowBackOutline/>
+      <TiHeartOutline />
+      <TiHeartFullOutline />
     </div>
   );
 }
